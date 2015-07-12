507e6344339bfc1a14b44ea69f483816d42c1d9b
Michael Lai <themichaellai@gmail.com>
2015-07-12 09:27:57 -0700

    Medical subscribing

--- parser/medical.js
+++ parser/medical.js
@@ -1,3 +1,5 @@
+var Subscriber = require('../models/subscriber');
+
 var factory = function(mongoose) {
   return function(body, cb) {
     var message = body.Body;
@@ -5,7 +7,23 @@ var factory = function(mongoose) {
     if (parts.length > 0 && parts[0] !== 'med') {
       return cb();
     }
-    return cb('help is on the way!');
+    if (parts[1] === 'sub') {
+      var subscriber = new Subscriber({
+        phoneNumber: body.From,
+        resource: 'med',
+      });
+      subscriber.save(function(err, res) {
+        return cb('you are subscribed for all med requests');
+      });
+    } else {
+      var order = parts.splice(1).join(' ');
+      Subscriber.find({
+        resource: 'med',
+      }, function(err, medPeople) {
+        // TODO: send texts to medPeople
+        return cb('help is on the way!');
+      });
+    }
   };
 };
 
